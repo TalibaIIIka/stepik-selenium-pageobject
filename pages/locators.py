093c3eb6031740b97535bdac1ab08f192984edc7
Anatoly Mikov <amikov@naumen.ru>
2019-07-28 19:43:08 +0500

    Chek login form and register form on login page

---
+++ pages/locators.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators(object):
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators(object):
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    LOGIN_EMAIL = (By.CSS_SELECTOR, '#id_login-username')
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, '#id_login-password')
+    LOGIN_BUTTON = (By.CSS_SELECTOR, 'button[name="login_submit"]')
+
+    REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,10 @@
+import pytest
+
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/'
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
@@ -8,8 +12,16 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
 
 
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_login_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
